5677aa81da00807269aabac032a6920013f915ec
quangduy28 <phanquangduytvt@gmail.com>
2022-11-30 02:13:06 +0700

    update

---
+++ .history/script/slider_20221129221847.js
@@ -0,0 +1,69 @@
+function showSlider() {
+  const slider = document.querySelector(".slider");
+  const sliderMain = document.querySelector(".slider-main");
+  const sliderItems = document.querySelectorAll(".slider-item");
+  const sliderPrev = document.querySelector(".slider-prev");
+  const sliderNext = document.querySelector(".slider-next");
+  const sliderWidth = sliderItems[0].offsetWidth;
+  const slidersLength = sliderItems.length;
+  // console.log(slidersLength);
+  let postionX = 0;
+  let index = 0;
+  sliderNext.addEventListener("click", function (e) {
+    console.log(sliderNext);
+    handleChangeSlide(1);
+  });
+  sliderPrev.addEventListener("click", function (e) {
+    console.log(sliderPrev);
+    handleChangeSlide(-1);
+  });
+
+  function handleChangeSlide(direction) {
+    console.log(index);
+    if (direction === 1) {
+      if (index >= slidersLength - 1) {
+        index = slidersLength - 1;
+        return;
+      }
+      postionX = postionX - sliderWidth;
+      sliderMain.style = `transform: translateX(${postionX}px)`;
+      index++;
+    } else if (direction === -1) {
+      if (index <= 0) {
+        index = 0;
+        return;
+      }
+      postionX = postionX + sliderWidth;
+      sliderMain.style = `transform: translateX(${postionX}px)`;
+      index--;
+    }
+  }
+}
+
+// thay đổi chuyển động***
+let index = 0;
+
+let n = 0;
+function imgAuto() {
+  const sliderMain = document.querySelector(".slider-main");
+  const sliderItems = document.querySelectorAll(".slider-item");
+  const sliderWidth = sliderItems[0].offsetWidth;
+
+  n = n + 1;
+
+  if (n <= 3) {
+    index = index + sliderWidth; // or index = index + silderWidth
+  } else {
+    index = index - sliderWidth;
+    if (n > 5) {
+      n = n * 0;
+    }
+  }
+  const vt = 0 - index;
+  sliderMain.style = `transform: translate3d(${vt}px,0px,0px)`;
+  // sliderMain.style = `width:${vt}px`;
+  console.log(vt);
+}
+setInterval(imgAuto, 1500);
+
+showSlider();
